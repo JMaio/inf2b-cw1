f97cd070a4025ea2c62470faf19affeb0c992f70
JMaio <j.f.maio98@gmail.com>
2018-02-25 20:35:48 +0000

    complete exercises

--- src/imult/StudentCode.java
+++ src/imult/StudentCode.java
@@ -1,5 +1,6 @@
 package imult;
 
+import java.io.File;
 import java.util.ArrayList;
 
 /*
@@ -65,6 +66,7 @@ public class StudentCode {
 	  int n = Math.max(A.length(), B.length());
 	  int mid = Math.floorDiv(n, 2);
 	  
+//	  obtain in reverse order due to representation
 	  BigInt alphaZero 	= A.split(0, mid-1);
 	  BigInt alphaOne 	= A.split(mid, n);
 	  
@@ -72,14 +74,10 @@ public class StudentCode {
 	  BigInt betaOne 	= B.split(mid, n);
 	  
 //	  base case - single multiplication
-	  boolean cond = A.length() == 1 && B.length() == 1;
+	  boolean cond = A.length() <= 1 || B.length() <= 1;
 	  
 	  if (cond) {
-		  DigitAndCarry dc = Arithmetic.mulDigits(alphaOne.getDigit(0), betaOne.getDigit(0));
-		  
-		  r.setDigit(0, dc.getDigit());
-		  r.setDigit(1, dc.getCarry());
-		  
+		  r = Arithmetic.schoolMul(alphaOne, betaOne);
 	  } else {
 		  
 		  BigInt tZero	= koMul(alphaZero, betaZero);
@@ -91,11 +89,15 @@ public class StudentCode {
 							  add(tZero, tTwo)
 							  );
 		  
-		  r = tZero;
-		  r.lshift(1);
-		  r = add(r, tOne);
-		  r.lshift(1);
+//		  System.out.println(tZero.value());
+//		  System.out.println(tOne.value());
+//		  System.out.println(tTwo.value());
+		  
 		  r = add(r, tTwo);
+		  r.lshift(mid);
+		  r = add(r, tOne);
+		  r.lshift(mid);
+		  r = add(r, tZero);
 		  
 	  }
 	  
@@ -104,29 +106,117 @@ public class StudentCode {
   }
 
   public static BigInt koMulOpt(BigInt A, BigInt B) {
-	  return null;
+			  
+	  BigInt r = new BigInt();
+	  
+//	  control for Opt variant:
+	  int min = Math.min(A.length(), B.length());
+	  
+	  if (min <= 10) {
+		  
+		  r = Arithmetic.schoolMul(A, B);
+		  
+	  } else {
+		  
+		  int n = Math.max(A.length(), B.length());
+		  int mid = Math.floorDiv(n, 2);
+		  
+//		  obtain in reverse order due to representation
+		  BigInt alphaZero 	= A.split(0, mid-1);
+		  BigInt alphaOne 	= A.split(mid, n);
+		  
+		  BigInt betaZero 	= B.split(0, mid-1);
+		  BigInt betaOne 	= B.split(mid, n);
+		  
+//		  base case - single multiplication
+		  boolean cond = A.length() <= 1 || B.length() <= 1;
+		  
+		  if (cond) {
+			  
+			  r = Arithmetic.schoolMul(alphaOne, betaOne);
+//			  
+//			  DigitAndCarry dc = Arithmetic.mulDigits(alphaOne.getDigit(0), betaOne.getDigit(0));
+//			  
+//			  r.setDigit(0, dc.getDigit());
+//			  r.setDigit(1, dc.getCarry());
+			  
+		  } else {
+			  
+			  BigInt tZero	= koMulOpt(alphaZero, betaZero);
+			  
+			  BigInt tTwo	= koMulOpt(alphaOne, betaOne);
+			  
+			  BigInt tOne	= sub(
+					  koMulOpt((add(alphaZero, alphaOne)), (add(betaZero, betaOne))),
+					  add(tZero, tTwo)
+					  );
+			  
+//		  System.out.println(tZero.value());
+//		  System.out.println(tOne.value());
+//		  System.out.println(tTwo.value());
+			  
+//			  tTwo.lshift(2 * mid);
+//			  tOne.lshift(mid);
+//			  
+//			  r = add(add(tZero, tOne), tTwo);
+			  
+			  r = add(r, tTwo);
+			  r.lshift(mid);
+			  r = add(r, tOne);
+			  r.lshift(mid);
+			  r = add(r, tZero);
+			  
+		  }
+		  
+	  }
+	  
+	  return r;
+	  
   }
 
   public static void main(String argv[]) throws java.io.FileNotFoundException {
 	  
-	  BigInt a = new BigInt("5 4321");
-	  BigInt b = new BigInt("9 8760");
+//	  BigInt a = new BigInt("5 4321");
+//	  BigInt b = new BigInt("9 8760");
 	  
-	  BigIntMul.mulTest(new Unsigned(1000), new Unsigned(1));
+//	  BigIntMul.mulTest(new Unsigned(1000), new Unsigned(1));
 	  
-//	  koMul(new BigInt("1000 0000"), new BigInt("1000 0000"));
-	  
-//	  a.print();
+//	  koMul(new BigInt("1000 0000"), new BigInt("1000 0000")).print();
 //	  
-//	  System.out.println(a.length());
+////	  a.print();
+////	  
+////	  System.out.println(a.length());
+////	  
+////	  a.split(0, 1).print();
 //	  
-//	  a.split(0, 1).print();
-	  
 //	  BigInt c = koMul(a, b);
 //	  c.print();
-	  
+//	  
+//	  System.out.println(BigIntMul.mulVerify(a, b, koMul(a, b)));
+//	  
 //	  
 //	  add(a, b).print();
 //	  sub(b, a).print();
+	  
+	  BigIntMul.getRunTimes(
+			  new Unsigned(1),
+			  new Unsigned(10),
+			  new Unsigned(90),
+			  new File("koMulOptTimes"),
+			  true);
+	  
+	  BigIntMul.getRatios(
+			  new Unsigned(1),
+			  new Unsigned(10),
+			  new Unsigned(90),
+			  new File("koMulOptRatios"),
+			  new Unsigned(100));
+	  
+	  
+	  BigIntMul.plotRunTimes(
+			  0.002041362534658618,
+			  0.0016832707345887528,
+			  new File("koMulOptTimes"));
+	  
   }
 } //end StudentCode class
